e5eeaa557a7aed2865c8a7c7e0c6a41d50c441df
justalinko <alinkokomansuby@gmail.com>
2018-09-28 15:09:48 +0700

    fixed

--- commander/4-routes.php
+++ commander/4-routes.php
@@ -54,6 +54,68 @@ Class Routes extends PaniCode_{
 			{
 				echo "[!] config : header_static , masih kosong anda harus mengisi nya secara manual.";
 				self::console('nano ./config/route.php');
+			}else{
+				echo "Set header_static ::";$route=trim(fgets(STDIN));
+				$droute=$CONFIG['route']['header_static'];
+				$rr=str_replace($droute,$route,@file_get_contents('config/route.php'));
+				if(@file_put_contents('config/route.php',$rr))
+				{
+					self::Message('success');
+				}else{
+					self::Message('failed');
+				}
+			}
+
+		}elseif($a=='3')
+		{
+			if(empty($CONFIG['route']['sidebar_static']))
+			{
+				echo "[!] config : sidebar_static , masih kosong anda harus mengisi nya secara manual.";
+				self::console('nano ./config/route.php');
+			}else{
+				echo "Set sidebar_static ::";$route=trim(fgets(STDIN));
+				$droute=$CONFIG['route']['sidebar_static'];
+				$rr=str_replace($droute,$route,@file_get_contents('config/route.php'));
+				if(@file_put_contents('config/route.php',$rr))
+				{
+					self::Message('success');
+				}else{
+					self::Message('failed');
+				}
+			}
+		}elseif($a=='4')
+		{
+			if(empty($CONFIG['route']['footer_static']))
+			{
+				echo "[!] config : footer_static , masih kosong anda harus mengisi nya secara manual.";
+				self::console('nano ./config/route.php');
+			}else{
+				echo "Set footer_static ::";$route=trim(fgets(STDIN));
+				$droute=$CONFIG['route']['footer_static'];
+				$rr=str_replace($droute,$route,@file_get_contents('config/route.php'));
+				if(@file_put_contents('config/route.php',$rr))
+				{
+					self::Message('success');
+				}else{
+					self::Message('failed');
+				}
+			}
+		}elseif($a=='5')
+		{
+			if(empty($CONFIG['route']['base_dir']))
+			{
+				echo "[!] config : base_dir , masih kosong anda harus mengisi nya secara manual.";
+				self::console('nano ./config/route.php');
+			}else{
+				echo "Set base_dir ::";$route=trim(fgets(STDIN));
+				$droute=$CONFIG['route']['base_dir'];
+				$rr=str_replace($droute,$route,@file_get_contents('config/route.php'));
+				if(@file_put_contents('config/route.php',$rr))
+				{
+					self::Message('success');
+				}else{
+					self::Message('failed');
+				}
 			}
 		}
 	}
